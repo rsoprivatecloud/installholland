d8e81b7a42d1da0b63317a7df8023d38e68328a3
rpawlik <ramsey.pawlik@rackspace.com>
2012-11-14 14:08:46 -0600

    added function for return code check, added getopts, added function for queit mode, cleaned up output

--- installholland.sh
+++ installholland.sh
@@ -4,10 +4,65 @@
 # This script creates a backup user in MySQL, installs and configures Holland,
 # and sets up a cron job to run Holland once a day.
 
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+Script to install and configure Holland for OpenStack Private Cloud Support. This script creates a backup user in MySQL, installs and configures Holland, and sets up a cron job to run Holland once a day.
+
+OPTIONS:
+-h Show this message
+-q Quiet
+
+EXAMPLE:
+sh $0 -q
+EOF
+}
+
+
+while getopts "hq" OPTION
+do
+case $OPTION in
+                h)
+                  usage
+                  exit 0
+                  ;;
+                q)
+                  QUIET="1"
+                  ;;
+                                                                                                                                           
+        esac
+done
+
+
+printext ()
+{
+
+if [ ! "$QUIET" = "1" ]
+then
+echo "$*"
+fi
+
+}
+
+
+returncheck ()
+{
+if [ $? -ne 0 ]; then
+        printext "$*"
+	exit 1
+else
+        printext "Success!"
+fi
+}
+
+
 # Create the replication user in mysql
-echo "Creating the MySQL replication user"
+printext "Creating the MySQL replication user."
 BKUSER="os_backup_user"
-USERPASS=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-12};echo;`
+USERPASS=$( tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)
 
 # If '$BKUSER' exists, drop and recreate with proper permissions and password
 USEREXISTS=`sudo mysql -e "SELECT COUNT(User) FROM mysql.user WHERE User='$BKUSER'" | grep [0-9]`
@@ -23,31 +78,21 @@ sudo mysql -e "FLUSH PRIVILEGES" > /dev/null
 # Verify backup user created successfully
 mysql -u$BKUSER -p$USERPASS -e "quit"
 
-if [ $? -ne 0 ]; then
-	echo "Error, $BKUSER not created succesfully.  Investigate manually"
-	exit 1
-else
-	echo "Success!"
-fi
+returncheck "Error, $BKUSER not created succesfully.  Investigate manually"
 
 # Install Holland
-echo "Installing Holland"
+printext "Installing Holland."
 
 OS_VERSION=`lsb_release -i | awk -F ':' '{print $2}' | cut -f2`"_"`lsb_release -r | awk -F ':' '{print $2}' | cut -f2`
-sudo wget http://download.opensuse.org/repositories/home:/holland-backup/x$OS_VERSION/Release.key -O - | sudo apt-key add -
-sudo echo "deb http://download.opensuse.org/repositories/home:/holland-backup/x$OS_VERSION/ ./" | sudo tee -a /etc/apt/sources.list.d/holland.list > /dev/null
+sudo wget -q http://download.opensuse.org/repositories/home:/holland-backup/x$OS_VERSION/Release.key -O - | sudo apt-key add - >/dev/null
+sudo echo "deb http://download.opensuse.org/repositories/home:/holland-backup/x$OS_VERSION/ ./" > /etc/apt/sources.list.d/holland.list >/dev/null 
 sudo apt-get update > /dev/null
 sudo apt-get install -y holland holland-common holland-mysqldump > /dev/null
 
 # Verify Holland was installed
 sudo which holland > /dev/null
 
-if [ $? -ne 0 ]; then
-	echo "Error:  Holland was not installed.  Investigate manually"
-	exit 1
-else
-	echo "Success!"
-fi
+returncheck "Error:  Holland was not installed.  Investigate manually"
 
 # Remove the default config if it exists
 if [ -f /etc/holland/backupsets/default.conf ]; then
@@ -55,15 +100,12 @@ if [ -f /etc/holland/backupsets/default.conf ]; then
 fi
 
 # Create an OpenStack mysqldump config
-echo "Creating OpenStack MySQL dump configuration for Holland"
-sudo holland mk-config --name=OpenStack mysqldump > /dev/null
+printext "Creating OpenStack MySQL dump configuration for Holland."
+sudo holland -q  mk-config --name=OpenStack mysqldump >/dev/null 2>&1
 
 OSCONFIG="/etc/holland/backupsets/OpenStack.conf"
 SOCKET=$(sudo grep -A 5 "\[client\]" /etc/mysql/my.cnf | grep socket | awk -F '=' '{print $2}' | cut -d ' ' -f2 | sed 's/\//\\\//g')
 PORT=$(sudo grep -A 5 "\[client\]" /etc/mysql/my.cnf | grep port | awk -F '=' '{print $2}' | cut -d ' ' -f2 | sed 's/\//\\\//g')
-
-echo $SOCKET
-echo $PORT
 
 sudo sed -i "s/backups-to-keep = 1/backups-to-keep = 7/" $OSCONFIG
 sudo sed -i "s/# user = \"\" # no default/user = $BKUSER/" $OSCONFIG
@@ -76,26 +118,24 @@ sudo sed -i "s/exclude-tables = ,/#exclude-tables = ,/" $OSCONFIG
 sudo sed -i "s/exclude-engines = ,/#exclude-engines = ,/" $OSCONFIG
 sudo sed -i "s/additional-options = ,/#additional-options = ,/" $OSCONFIG
 
-echo "Success!"
+printext "Success!"
 
 # Create cron job to run Holland 1x per day
-echo "Creating daily cron job for Holland"
+printext "Creating daily cron job for Holland."
 sudo echo '#!/bin/bash
 holland backup OpenStack
 ' > /etc/cron.daily/holland
 
 if [ -f /etc/cron.daily/holland ]; then
 	sudo chmod +x /etc/cron.daily/holland
-	echo "Success!"
+	printext "Success!"
 else
 	echo "Failed to create cron job.  Investigate manually."
 fi
 
-# Perform dry run of the backup
-sudo holland bk -n OpenStack
 
-if [ $? -ne 0 ]; then
-	echo "Dry Run did not complete successfully.  Investigate manually."
-else
-	echo "Success!"
-fi
+printext "Performing dry run to verify installation."
+# Perform dry run of the backup
+sudo holland -q bk -n OpenStack 
+
+returncheck "Dry Run did not complete successfully. Investigate manually."
